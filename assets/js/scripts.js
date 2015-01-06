b5a394a2adc3a185948940ba69c9ec8161714aa9
brianmaierjr <brimaidesigns@gmail.com>
2015-01-05 21:03:30 -0500

    added reading time on posts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -8,6 +8,11 @@ $( document ).ready(function() {
 	// Responsive-Nav
 	var nav = responsiveNav(".nav-collapse");
 
+	// Round Reading Time
+    $(".time").text(function (index, value) {
+      return Math.round(parseFloat(value));
+    });
+
 });
 
 
