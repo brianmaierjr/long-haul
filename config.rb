88928670c522a17608150bf7f4b3c9ab6fed0190
brianmaierjr <brimaidesigns@gmail.com>
2015-04-15 22:00:29 -0400

    edit config.rb

--- config.rb
+++ config.rb
@@ -3,11 +3,11 @@ require "normalize-scss"
 require "susy"
 
 # Set this to the root of your project when deployed:
-http_path = "/long-haul/"
-css_dir = "/long-haul/assets/css"
-sass_dir = "/long-haul/assets/scss"
-images_dir = "/long-haul/assets/img"
-javascripts_dir = "/long-haul/assets/js"
+http_path = "/long-haul/" 
+css_dir = "assets/css"
+sass_dir = "assets/scss"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
